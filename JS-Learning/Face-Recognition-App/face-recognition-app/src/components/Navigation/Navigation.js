74575574be64395e9eb53ecbea45616dfd49e8e8
Abdullah-Sahraoui <73436338+Abdullah-Sahraoui@users.noreply.github.com>
2020-12-03 18:42:13 +0000

    Added a route for app.
    Added signin and register forms.

--- JS-Learning/Face-Recognition-App/face-recognition-app/src/components/Navigation/Navigation.js
+++ JS-Learning/Face-Recognition-App/face-recognition-app/src/components/Navigation/Navigation.js
@@ -1,13 +1,19 @@
 import React from 'react';
 
-const Navigation = () => {
-	return(
-		<div className="">
-			<nav style={{display: 'flex', justifyContent: 'flex-end'}}>
-				<p className="f3 link dim black underline pa3 pointer allow-pointer-events">Sign out</p>
-			</nav>
-		</div>
-	);
+const Navigation = ({ onRouteChange, isSignedIn }) => {
+		if(isSignedIn) {
+			return(
+				<nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+					<p onClick={() => onRouteChange('signout')} className="f3 link dim black underline pa3 pointer allow-pointer-events">Sign out</p>
+				</nav>)
+		} else {
+				return (
+					<nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+						<p onClick={() => onRouteChange('signin')} className="f3 link dim black underline pa3 pointer allow-pointer-events">Sign in</p>
+						<p onClick={() => onRouteChange('Register')} className="f3 link dim black underline pa3 pointer allow-pointer-events">Register</p>
+					</nav>
+				);
+			}
 }
 
 export default Navigation;
