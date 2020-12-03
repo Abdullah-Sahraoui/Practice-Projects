c5c093c1c5cbebc5215cb401081becef0e705804
Abdullah-Sahraoui <73436338+Abdullah-Sahraoui@users.noreply.github.com>
2020-12-03 15:08:55 +0000

    Established connection to Clarifai API and receive response
    Created component to display image from input box.

--- JS-Learning/Face-Recognition-App/face-recognition-app/src/components/ImageLinkForm/ImageLinkForm.js
+++ JS-Learning/Face-Recognition-App/face-recognition-app/src/components/ImageLinkForm/ImageLinkForm.js
@@ -7,7 +7,7 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
 			<p className="f3 center fw6">
 				{'This app detects any faces in your pictures. Enter your image URL below to get started!'}
 			</p>
-			<div className="center input-container">
+			<div className="center">
 				<div className="center pa4 form ba br2 b--orange allow-pointer-events">
 					<input className="f4 pa2 w-70 center" type="text" placeholder="Enter URL here" onChange={onInputChange}/>
 					<button className="w-30 grow f4 link ph3 pv2 dib white bg-pink shadow" onClick={onButtonSubmit} >Detect</button>
--- JS-Learning/Face-Recognition-App/face-recognition-app/src/containers/App.js
+++ JS-Learning/Face-Recognition-App/face-recognition-app/src/containers/App.js
@@ -2,9 +2,15 @@ import React, { Component } from 'react';
 import Navigation from '../components/Navigation/Navigation';
 import Logo from '../components/Logo/Logo';
 import ImageLinkForm from '../components/ImageLinkForm/ImageLinkForm';
+import FaceRecognition from '../components/FaceRecognition/FaceRecognition';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 import './App.css';
 import '../components/Logo/Logo.css';
+
+const app = new Clarifai.App({
+	apiKey: '30651752ca9b4da8b52993b6aae32638'
+});
 
 const particlesSettings = {
 	"particles": {
@@ -39,15 +45,25 @@ class App extends Component {
 		super();
 		this.state = {
 			input: '',
+			imageUrl: ''
 		}
 	}
 	
 	onInputChange = (event) => {
-		console.log(event.target.value);
+		this.setState({input: (event.target.value)});
 	}
 
 	onButtonSubmit = () => {
-		console.log('click');
+		this.setState({imageUrl: this.state.input})
+		app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+			.then(
+			  function(response) {
+			    console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+			  },
+			  function(err) {
+			    // there was an error
+			  }
+			);
 	}
 
 
@@ -58,6 +74,7 @@ class App extends Component {
 				<Navigation />
 				<Logo />
 				<ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
+				<FaceRecognition imageUrl={this.state.imageUrl}/>
 			</div>
 		);
 	}
